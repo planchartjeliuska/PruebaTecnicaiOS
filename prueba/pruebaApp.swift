c0ee0069a308739f03fadf97963bd2a2b5407e66
jeliuska <planchartjeliuska@gmail.com>
2024-04-14 11:28:09 -0500

    Initial Commit

---
+++ prueba/pruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaApp.swift
+//  prueba
+//
+//  Created by Jeliuska on 14/04/24.
+//
+
+import SwiftUI
+
+@main
+struct pruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
